05cc12ba66e822caf10eae9848d016b03d357459
Gerardo Santillana <gerardo.santillana@legrand.us>
2017-02-27 14:14:55 -0500

    Initial commit

---
+++ server.js
@@ -0,0 +1,72 @@
+const express = require('express');  //get if from https://www.npmjs.com/package/express   doc //www.expressjs.com
+const hbs = require('hbs');  //get if from https://www.npmjs.com/package/hbs  doc http://handlebarsjs.com/
+
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view-engine', 'hbs');    //views is a default folder that express use for all the templates
+
+app.use((req, res, next) => {
+  var now = new Date().toString();
+  var log = `${now}: ${req.method} ${req.url}`;
+
+  console.log(log);
+  fs.appendFile('server.log', log + '\n', (err) => {
+    if (err) {
+      console.log('Unable to append to server.log');
+    }
+  });
+  next();  // calling next you tell express that the middleware has finished
+});
+
+//this function is to display when the  website  is  on maintenance, not allowing  run the other webpages
+
+// app.use((req, res, next) => {
+//   res.render('maintenance.hbs');
+// });
+
+app.use(express.static(__dirname +  '/public'));   //with USE you register a middleware
+
+
+hbs.registerHelper('getCurrentYear', () => {
+  return new Date().getFullYear()
+});
+
+
+hbs.registerHelper('screamIt', (text) => {
+  return text.toUpperCase();
+});
+
+app.get('/', (req, res) => {
+  // res.send ('<h1>Hello express</h1>');
+  // res.send({
+  //   name: 'Gerry',
+  //   likes: [
+  //     'Movies',
+  //     'Jogging'
+  //   ]
+  // });
+  res.render('home.hbs', {
+    pageTitle: 'Home Page',
+    welcomeMessage: 'Welcome to my website'
+  });
+});
+
+app.get('/about', (req, res) => {
+//   res.send ('About  page!');
+  res.render('about.hbs', {
+    pageTitle: 'About Page'
+  });
+});
+
+app.get('/bad', (req, res) => {
+  res.send ({
+    errorMessage: 'Unable to handle request'
+  });
+});
+
+app.listen(3000, () => {
+  console.log('Server is up on port 3000');
+});
