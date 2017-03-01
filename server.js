528ea2ac6a7db4884128222ac34a706d01ad83c4
Gerardo Santillana <jgsantillana@hotmail.com>
2017-02-28 22:52:09 -0500

    Add project  page

--- server.js
+++ server.js
@@ -62,6 +62,15 @@ app.get('/about', (req, res) => {
   });
 });
 
+app.get('/projects', (req, res) => {
+//   res.send ('About  page!');
+  res.render('projects.hbs', {
+    pageTitle: 'Projects Page',
+    welcomeMessage: 'Project page text would go here'
+  });
+});
+
+// send back json with errorMessage
 app.get('/bad', (req, res) => {
   res.send ({
     errorMessage: 'Unable to handle request'
